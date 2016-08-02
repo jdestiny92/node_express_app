1a9ccb8314a4b50934e0e936ae3ff0fc5f60d07f
Julian Biebelberg <jbiebelberg@gmail.com>
2016-08-02 17:43:52 -0400

    added most of functionality, need to implement routes

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var classes = ['dark knight', 'ranger', 'archer', 'samurai', 'paladin', 'summoner', 'cleric', 'alchemist', 'sorcerer'];
+var classes = ['darkknight', 'ranger', 'archer', 'samurai', 'paladin', 'summoner', 'cleric', 'alchemist', 'sorcerer'];
 
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
 app.get('/survey', function(req, res) {
+	console.log('hello');
     res.sendfile('survey.html');
 });
 
 app.get('/:class', function(req, res) {
     var chosen = req.params.class;
 
-	console.log(chosen);
+	for(i=0; i<classes.length; i++){
+		if(chosen == classes[i]){
+			res.sendfile(classes[i] + '.html');
+		}
+	}
+});
+
+app.get('*', function(req, res) {
+	res.sendfile('404.html');
 });
 
 
